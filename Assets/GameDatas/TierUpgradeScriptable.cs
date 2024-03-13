7f9fd29490920117720220c2011a485f3249c11d
martin-newbie <g.martin.min@gmail.com>
2024-03-13 22:25:53 +0900

    티어 데이터 추가

--- Assets/GameDatas/TierUpgradeScriptable.cs
+++ Assets/GameDatas/TierUpgradeScriptable.cs
@@ -5,5 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TierData", menuName = "GameDatas/TierData", order = int.MinValue)]
 public class TierUpgradeScriptable : DataScriptable
 {
+    public TierData[] datas;
+}
 
+[System.Serializable]
+public class TierData
+{
+    public int item_require;
+    public int coin_require;
 }
