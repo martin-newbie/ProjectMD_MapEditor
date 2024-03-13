53a9a09079331ad3add03e94d6f70399c82ab927
martin-newbie <g.martin.min@gmail.com>
2024-03-13 21:53:21 +0900

    폴더 정리, 스킬 아이템 데이터 관리 방식 변경

--- Assets/GameDatas/SkillItemScriptable.cs
+++ Assets/GameDatas/SkillItemScriptable.cs
@@ -5,14 +5,14 @@ using System.IO;
 #if UNITY_EDITOR
 using UnityEditor;
 
-[CustomEditor(typeof(CommonSkillItemScriptable), true)]
+[CustomEditor(typeof(SkillItemScriptable), true)]
 public class CommonSkillLoadButton : Editor
 {
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
 
-        CommonSkillItemScriptable item = target as CommonSkillItemScriptable;
+        SkillItemScriptable item = target as SkillItemScriptable;
         if (GUILayout.Button("Export"))
         {
             item.ExportToData();
@@ -22,15 +22,18 @@ public class CommonSkillLoadButton : Editor
 #endif
 
 
-[CreateAssetMenu(fileName = "commonSkillItem", menuName = "skillData/commonSkill", order = int.MinValue)]
-public class CommonSkillItemScriptable : ScriptableObject
+[CreateAssetMenu(fileName = "skillItem", menuName = "skillData/skillItem", order = int.MinValue)]
+public class SkillItemScriptable : ScriptableObject
 {
+
+    public string title;
+
     [SerializeField]
     public SkillItemData[] skillItemDatas;
 
     public void ExportToData()
     {
-        string resourcePath = "SkillItem/CommonSkill";
+        string resourcePath = $"SkillItem/{title}";
         string dataPath = Application.dataPath + "/Resources/" + resourcePath + ".txt";
         string jsonData = JsonUtility.ToJson(this);
         File.WriteAllText(dataPath, jsonData);
@@ -41,6 +44,7 @@ public class CommonSkillItemScriptable : ScriptableObject
 [System.Serializable]
 public class SkillItemData
 {
+    public int coin;
     public ItemData[] items;
 }
 
